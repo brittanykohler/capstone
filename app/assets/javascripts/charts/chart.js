d08911df240b9c978de430a8dd636725ce54044c
Brittany <BrittanyKohler@gmail.com>
2016-03-06 21:04:01 -0800

    Commenting out chart code for now

--- app/assets/javascripts/charts/chart.js
+++ app/assets/javascripts/charts/chart.js
@@ -6,28 +6,28 @@
 // // Create the chart
 // Highcharts.chart('chart', { /*Highcharts options*/ });
 //
-$(function () {
-    $('#chart').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Weekly Steps'
-        },
-        xAxis: {
-            categories: ['M', 'T', 'W', 'Th', 'F', 'S', 'S']
-        },
-        yAxis: {
-            title: {
-                text: 'Steps'
-            }
-        },
-        series: [{
-          showInLegend: false,
-          data: [10000, 400, 6500, 3000, 2900]
-        }],
-        credits: {
-            enabled: false
-        },
-    });
-});
+// $(function () {
+//     $('#chart').highcharts({
+//         chart: {
+//             type: 'column'
+//         },
+//         title: {
+//             text: 'Weekly Steps'
+//         },
+//         xAxis: {
+//             categories: ['M', 'T', 'W', 'Th', 'F', 'S', 'S']
+//         },
+//         yAxis: {
+//             title: {
+//                 text: 'Steps'
+//             }
+//         },
+//         series: [{
+//           showInLegend: false,
+//           data: [10000, 400, 6500, 3000, 2900]
+//         }],
+//         credits: {
+//             enabled: false
+//         },
+//     });
+// });
