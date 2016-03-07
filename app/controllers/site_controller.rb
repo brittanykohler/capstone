d34ca90f7585c2985b743e35c88072f0976cfe1d
Brittany <BrittanyKohler@gmail.com>
2016-03-07 13:37:06 -0800

    Adding instance variables for trips in site controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -53,4 +53,20 @@ class SiteController < ApplicationController
       gon.step_goal = @step_goal
     end
   end
+
+  def trips
+    if logged_in?
+      @current_steps = current_user.get_current_steps
+      @step_goal = current_user.get_step_goal
+      if @step_goal > @current_steps
+        @steps_needed = @step_goal - @current_steps
+      else # daily step goal is completed
+        @steps_needed = 1250 # update this later
+      end
+      # distance is in meters
+      @distance_needed = @steps_needed * current_user.stride_length_walking.to_f / 100
+      gon.distance_needed = @distance_needed
+      gon.stride_length_walking = current_user.stride_length_walking
+    end
+  end
 end
