329c74a8edcbadd183f08e74dd49f0a84c4d7b73
Brittany <BrittanyKohler@gmail.com>
2016-03-07 11:41:43 -0800

    Updating get_steps_for_week to use utc time and user offset

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
     user = self.find_by(u_id: auth["uid"])
     if !user.nil?
       # User found in db
+      if user.user_token != auth['credentials']['token'] && user.user_secret != auth['credentials']['secret']
+        user.user_token = auth['credentials']['token']
+        user.user_secret = auth['credentials']['secret']
+        user.save
+      end
       return user
     else
       # create new user in db
@@ -11,6 +16,7 @@ class User < ActiveRecord::Base
       user.u_id                   = auth["uid"]
       user.name                   = auth["info"]["name"]
       user.timezone               = auth["info"]["timezone"]
+      user.offset_from_utc_millis = auth["extra"]["raw_info"]["user"]["offsetFromUTCMillis"]
       user.stride_length_walking  = auth["extra"]["raw_info"]["user"]["strideLengthWalking"]
       user.stride_length_running  = auth["extra"]["raw_info"]["user"]["strideLengthRunning"]
       user.photo                  = auth["extra"]["raw_info"]["user"]["avatar"]
@@ -34,7 +40,9 @@ class User < ActiveRecord::Base
     client.reconnect(self.user_token, self.user_secret)
 
     # client.activities_on_date('2015-03-25') <- Specific Date
-    info = client.activities_on_date('today')
+    today = Time.now.utc + (self.offset_from_utc_millis / 1000) # convert difference to seconds
+    formatted_today = today.strftime('%Y-%m-%d')
+    info = client.activities_on_date(formatted_today)
     current_steps = info["summary"]["steps"]
     return current_steps
   end
@@ -71,7 +79,7 @@ class User < ActiveRecord::Base
 
     week_data = []
     weekdays = []
-    day = Time.now
+    day = Time.now.utc + (self.offset_from_utc_millis / 1000)
     7.times do
       weekdays << day.strftime("%a")
       day_formatted = day.strftime("%Y-%m-%d")
