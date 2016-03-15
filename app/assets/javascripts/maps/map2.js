5eefb31052c7b194047619bd5e11a62d73918db2
Brittany <BrittanyKohler@gmail.com>
2016-03-15 11:36:10 -0700

    fixing bad string concatenation in map2.js

--- app/assets/javascripts/maps/map2.js
+++ app/assets/javascripts/maps/map2.js
@@ -173,8 +173,7 @@ function listPlaces(destinations) {
 }
 
 function addToPage(sortedDestinations) {
-  $(".places").append("<div class='results-display'> <div class='results-hex hex1 orange-hex'>" + gon.trip_type.toUpperCase() + "</div>" + "<div class='results-hex hex1'>" + gon.place_type.toUpperCase() + "</div>"
-   + "<div class='results-hex hex1 teal-hex'>STEPS: " + gon.steps_needed + "</div></div>");
+  $(".places").append("<div class='results-display'> <div class='results-hex hex1 orange-hex'>" + gon.trip_type.toUpperCase() + "</div><div class='results-hex hex1'>" + gon.place_type.toUpperCase() + "</div><div class='results-hex hex1 teal-hex'>STEPS: " + gon.steps_needed + "</div></div>");
   for (var i = 0; i < sortedDestinations.length; i++) {
     $(".places").append("<p class='place" + i + " place-box'>" + sortedDestinations[i].name + " - STEPS: " + sortedDestinations[i].steps + "</p>");
     if (gon.trip_type === "Round-trip") {
