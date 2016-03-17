798aa3e13d82287c5d53aa0d80ebbdc366119c7f
Brittany <BrittanyKohler@gmail.com>
2016-03-17 15:35:00 -0700

    Adding test for get_steps_for_week in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,4 +66,25 @@ RSpec.describe User, type: :model do
       expect(goal).to eq("10000")
     end
   end
+
+  describe "get_steps_for_week" do
+    before :each do
+      @auth_hash = OmniAuth::AuthHash.new({:uid => ENV["FITBIT_UID"],
+        info: {name: "Brittany", timezone: "LosAngeles"},
+        extra: {raw_info: {user: {strideLengthWalking: "72", strideLengthRunning: "95", avatar: "www.google.com", offsetFromUTCMillis: -28800000}}},
+        credentials: {token: ENV["FITBIT_USER_TOKEN"], secret: ENV["FITBIT_USER_SECRET"]}})
+    end
+
+    it "returns week data and associated weekdays" do
+      client = double(Fitgem::Client)
+      allow(client).to receive(:reconnect)
+      allow(client).to receive(:activity_on_date_range).and_return({"activities-steps" => [{"dateTime" => "03-08-1989", "value" => 123}]})
+      allow_any_instance_of(User).to receive(:get_fitbit_client).and_return(client)
+
+      user = User.find_or_create_from_omniauth(@auth_hash)
+      week_data, weekdays = user.get_steps_for_week
+      expect(week_data).to eq([123])
+      expect(weekdays).to eq(["Thursday"])
+    end
+  end
 end
