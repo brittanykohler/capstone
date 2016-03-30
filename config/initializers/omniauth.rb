222e8c0fe9fa8070affea1e803e06f6e35d2663a
Brittany <BrittanyKohler@gmail.com>
2016-03-30 15:31:20 -0700

    Updating redirect URI

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :fitbit, ENV['FITBIT_OAUTH2_CLIENT_ID'], ENV['FITBIT_CLIENT_SECRET'], { :scope => 'profile activity', :redirect_uri => 'http://localhost:3000/auth/fitbit/callback' }
+  provider :fitbit, ENV['FITBIT_OAUTH2_CLIENT_ID'], ENV['FITBIT_CLIENT_SECRET'], { :scope => 'profile activity', :redirect_uri => 'http://fittrip.herokuapp.com/auth/fitbit/callback' }
 end
